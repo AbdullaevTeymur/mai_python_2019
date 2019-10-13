d79e3aa12e0ac6e34012a092ee59a92da841d443
AbdullaevTeymur <56480386+AbdullaevTeymur@users.noreply.github.com>
2019-10-13 19:17:33 +0300

    Update hw01.py

--- 01 Intro/hw01.py
+++ 01 Intro/hw01.py
@@ -12,8 +12,8 @@ import csv
 # читаем информацию о квартирах в список flats_list
 flats_list = list()
 with open('output.csv', encoding="utf-8") as csvfile:
-	flats_csv = csv.reader(csvfile, delimiter=';')
-	flats_list = list(flats_csv)
+    flats_csv = csv.reader(csvfile, delimiter=';')
+    flats_list = list(flats_csv)
 
 # убираем заголовок
 header = flats_list.pop(0)
@@ -21,20 +21,21 @@ header = flats_list.pop(0)
 # создаем словарь с информацией о квартирах
 subway_dict = {}
 for flat in flats_list:
-	subway = flat[3].replace("м.", "")
-	subway_dict.setdefault(subway, [])
+    if flat[3] != "":
+        subway = flat[3].replace("м.", "")
+        subway_dict.setdefault(subway, [])
 # TODO 1: добавьте код, который генерирует новую структуру данных с информацией о квартире - словарь вместо списка
-    # ваш код...
     # не забудьте сделать проверку типа и преобразовать то, что можно, в числа
-    flat_info = ....
-
-	subway_dict[subway].append(flat_info)
+        if flat[0].isdigit() and flat[1].isdigit() and flat[11].isdigit():
+            flat_info = {"id": int(flat[0]), "rooms": int(flat[1]), "type": flat[2], "price": int(flat[11])}
+            subway_dict[subway].append(flat_info)
 
 # TODO 2: подсчитайте и выведите на печать количество новостроек, расположенных рядом с каждым из метро. Используйте вариант прохода по словарю, который вам больше нравится
-for k,v in subway_dict:
+for k, v in subway_dict.items():
+    count = 0
     # используйте v для анализа значения
-    # ваш код...
-# либо
-for k in subway_dict:
-    # используйте subway_dict[k] для анализа значения
-    # ваш код...
+    for flat in v:
+        for key, value in flat.items():
+            if key == "type" and value.lower() == "новостройка":
+                count += 1
+    print(f"У метро {k} расположено новостроек в количестве {count} шт.")
